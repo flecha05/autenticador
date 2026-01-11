fb2905675e467a136ed9578345e1aa77e3f74be4
FlechaU <48757845+flecha05@users.noreply.github.com>
2026-01-11 14:41:39 +0000

    Prueba funcionalidades luego de recuperar, falla mostrar ctrs

--- main.cpp
+++ main.cpp
@@ -7,31 +7,32 @@
 using namespace std;
 
 int main(){
-    /*
-    string nombreUsuario;
-    
-    cout<<"----------GESTOR DE CONTRASENAS----------"<<endl;
-    //Solicito el nombre del usuario para cargar sus archivos
-    cout<<"Usuario: ";
-    getline(cin,nombreUsuario);
-    string authPass = nombreUsuario+"_auth.dat";
-    string contrasenas = nombreUsuario + "_contras.dat";
-
-    //Autentifico al usuario con su contrasena maestra o se crea contrasena
-    Autenticador auth(authPass);
-    if(!auth.autenticar()){
-        cout<<"Limite de intentos. Saliendo del sistema"<<endl;
-        return 1;
-    }
-    //YA SE INGRESO AL SISTEMA_ IMPLEMENTAR MENU DE GESTION DE CONTRASENAS
-
-    menuPrincipal(nombreUsuario, contrasenas);
-*/
+   
+   
 try {
         Autenticador auth("auth.dat");
 
         if (auth.autenticar()) {
             std::cout << "Autenticacion exitosa\n";
+             string nombreUsuario;
+    
+            cout<<"----------GESTOR DE CONTRASENAS----------"<<endl;
+            //Solicito el nombre del usuario para cargar sus archivos
+            cout<<"Usuario: ";
+            getline(cin,nombreUsuario);
+            string authPass = nombreUsuario+"_auth.dat";
+            string contrasenas = nombreUsuario + "_contras.dat";
+
+            //Autentifico al usuario con su contrasena maestra o se crea contrasena
+            Autenticador auth(authPass);
+            if(!auth.autenticar()){
+                cout<<"Limite de intentos. Saliendo del sistema"<<endl;
+                return 1;
+            }
+            //YA SE INGRESO AL SISTEMA_ IMPLEMENTAR MENU DE GESTION DE CONTRASENAS
+
+            menuPrincipal(nombreUsuario, contrasenas);
+
         } else {
             std::cout << "Password incorrecta\n";
         }
